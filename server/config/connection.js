2fbab29b3ab1366eff3ba9682a2aac468c6868d7
dhclarke99 <dhclarke99@gmail.com>
2023-12-15 19:17:32 -0800

    server.js and config.js starter code

---
+++ server/config/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../.env'});
+const mongoose = require('mongoose');
+
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+module.exports = mongoose.connection;
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+require('dotenv').config({ path: '../.env' });
+const api = require('./routes/index.js');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve image assets
+app.use('/images', express.static(path.join(__dirname, '../web-client/images')));
+
+// Define routes
+app.use('/api', api);
+
+// Initialize Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Start Apollo Server
+const startApolloServer = async () => {
+  await server.start();
+  // Apply Apollo GraphQL middleware and set the path to /graphql
+  server.applyMiddleware({ app });
+
+  // Serve static files from the React app
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../web-client/build')));
+  }
+
+  // All other GET requests not handled before will return the React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../web-client/build/index.html'));
+  });
+
+  // Start the MongoDB connection
+  db.once('open', () => {
+    // Start the Express server
+    app.listen(PORT, () => {
+      console.log('Successfully connected to the database');
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer();
