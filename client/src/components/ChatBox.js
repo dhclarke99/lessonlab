a0c9c67fe607c7868331f805124d8591dab45e20
dhclarke99 <dhclarke99@gmail.com>
2023-12-16 14:26:47 -0800

    chatbox styling

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -4,8 +4,10 @@ import '../utils/css/ChatBox.css';
 const ChatBox = () => {
     return (
         <div className="chat-container">
-            <input type="text" className="chat-input" placeholder="Type your message here..." />
-            <button className="send-button">Send</button>
+            <div className="input-container">
+                <input type="text" className="chat-input" placeholder="Type your message here..." />
+                <button className="send-button"></button>
+            </div>
         </div>
     );
 }
--- client/src/pages/StepOne.js
+++ client/src/pages/StepOne.js
@@ -6,7 +6,16 @@ const StepOne = () => {
     return (
         <div className="step-one-container">
             <Intro />
+            <div classname='secondary-container'>
+            <div classname='step-one-content'>
+                <header>Lesson Lab</header>
+                <h1>Step 1: Describe an objective that matters to you.</h1>
+                <p>What is a skill that your students find difficult to attain?</p>
+                <p>Which academic standard do students struggle to meet?</p>
+                <button>See examples</button>
+            </div>
             <ChatBox />
+            </div>
            
         </div>
     );
