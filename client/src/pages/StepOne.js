7b31b4684fda0ddddfa786c5183625971087873c
dhclarke99 <dhclarke99@gmail.com>
2023-12-16 14:59:34 -0800

    updated chatbox styles

--- client/src/pages/StepOne.js
+++ client/src/pages/StepOne.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import '../utils/css/StepOne.css';
 import Intro from '../components/Intro'
 import ChatBox from '../components/ChatBox'
+import '../utils/css/StepOne.css';
+
 const StepOne = () => {
     return (
         <div className="step-one-container">
