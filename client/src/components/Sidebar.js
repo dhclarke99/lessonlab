74a26d0db9f77bed642a743f68d50456a269ea55
dhclarke99 <dhclarke99@gmail.com>
2023-12-15 21:48:13 -0800

    sidebar styles

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import '../utils/css/Sidebar.css';
+import measuringCup from '../assets/images/measuringcup.jpeg'
 
 const Sidebar = () => {
+    const userInitials = "M"; // Replace with logic to get logged-in user's initials
+
     return (
         <div className="sidebar-component">
-            <div className="sidebar-icon top">New Message</div>
-            <div className="sidebar-icon bottom">Profile</div>
+            <div className="sidebar-icon top">
+                <img src={measuringCup} alt="Icon" className="icon-image" />
+                New Experiment
+            </div>
+            <div className="sidebar-icon bottom">
+                <span className="icon-initials">{userInitials}</span>
+                Profile
+            </div>
         </div>
     );
 }
