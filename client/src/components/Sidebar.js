5870e38884864af4ebe5482dc9d1868b4567f7d6
dhclarke99 <dhclarke99@gmail.com>
2023-12-17 15:18:43 -0800

    sidebar styles

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../utils/css/Sidebar.css';
-import measuringCup from '../assets/images/measuringcup.jpeg'
-import { useQuery, } from '@apollo/client';
+import measuringCup from '../assets/images/measuringcup.jpeg';
+import { useQuery } from '@apollo/client';
 import { GET_USER_BY_ID } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Sidebar = () => {
+    const [showLogout, setShowLogout] = useState(false);
     const { loading: userLoading, error: userError, data: userData } = useQuery(GET_USER_BY_ID, {
         variables: { userId: Auth.getProfile().data._id },
-      });
+    });
 
-      const handleLogout = async () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const handleLogout = async () => {
         Auth.logout();
-        
-      };
-  
-      if (userLoading) return <p>Loading...</p>;
-      if (userError) return <p>Error: {userError.message}</p>;
-      console.log(userData)
-      const firstInitial = userData.user.firstname[0]
-    
+    };
+
+    if (userLoading) return <p>Loading...</p>;
+    if (userError) return <p>Error: {userError.message}</p>;
+    const firstInitial = userData.user.firstname[0];
 
     return (
         <div className="sidebar-component">
@@ -27,11 +31,20 @@ const Sidebar = () => {
                 <img src={measuringCup} alt="Icon" className="icon-image" />
                 New Experiment
             </div>
-            <div className="sidebar-icon bottom">
+            <div>
+            {showMenu && (
+                    <div className="profile-menu">
+                        <div className="menu-item">Settings</div>
+                        <div className="menu-item" onClick={handleLogout}>Logout</div>
+                    </div>
+                )}
+            <div className="sidebar-icon bottom" onClick={toggleMenu}>
                 <span className="icon-initials">{firstInitial}</span>
                 Profile
+                
             </div>
-            <button onClick={handleLogout}>Logout</button>
+            </div>
+            
         </div>
     );
 }
