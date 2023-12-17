ee9e755854ee022e581f6ac2feef126b6062038e
dhclarke99 <dhclarke99@gmail.com>
2023-12-17 15:34:06 -0800

    dropdown for grade level

--- client/src/pages/GetStarted.js
+++ client/src/pages/GetStarted.js
@@ -11,7 +11,25 @@ const GetStarted = ({ onGetStartedClick }) => {
             <Intro />
                 <div className="input-group">
                     <label>I teach</label>
-                    <input type="text" placeholder="grade level" />
+                    <select type="text"
+                name="gradeLevel"
+                placeholder='grade level'
+                
+                >
+                  <option value='' disabled>Select One</option>
+                <option value="1st Grade">1st Grade</option>
+                <option value="1st Grade">2nd Grade</option>
+                <option value="1st Grade">3rd Grade</option>
+                <option value="1st Grade">4th Grade</option>
+                <option value="1st Grade">5th Grade</option>
+                <option value="1st Grade">6th Grade</option>
+                <option value="1st Grade">7th Grade</option>
+                <option value="1st Grade">8th Grade</option>
+                <option value="1st Grade">9th Grade</option>
+                <option value="1st Grade">10th Grade</option>
+                <option value="1st Grade">11th Grade</option>
+                <option value="1st Grade">12th Grade</option>
+              </select>
                     <input type="text" placeholder="subject area" />
                     <p>.</p>
                 </div>
