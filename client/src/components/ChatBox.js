f18f24f1ea5b058632615bd1a0e98a3cba01d68c
dhclarke99 <dhclarke99@gmail.com>
2023-12-24 12:58:21 -0800

    clear chatbox on submit

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -4,14 +4,16 @@ import { UPDATE_USER } from '../utils/mutations';
 import '../utils/css/ChatBox.css';
 
 const ChatBox = ({ onStepOneClick }) => {
+    const [inputValue, setInputValue] = useState('');
     const id = localStorage.getItem('userId')
     const [formData, setFormData] = useState({
         getStartedPrompts: [],
-        
+
     });
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: [e.target.value] });
+        setInputValue(e.target.value); // Update the inputValue state
+  
     };
 
     const [updateUser] = useMutation(UPDATE_USER);
@@ -19,24 +21,36 @@ const ChatBox = ({ onStepOneClick }) => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
-            console.log(formData)
-          const { data } = await updateUser({
-            variables: { userId: id, input: { ...formData } },
-          });
-    console.log(data)
-    onStepOneClick();
+            // Update formData.getStartedPrompts with the current inputValue
+            const updatedPrompts = [...formData.getStartedPrompts, inputValue];
+
+            const { data } = await updateUser({
+                variables: { userId: id, input: { ...formData, getStartedPrompts: updatedPrompts } },
+            });
+            console.log(data);
+
+            // Clear the input field
+            setInputValue("");
+
+            onStepOneClick();
+
         } catch (e) {
-          console.error(e);
+            console.error(e);
         }
-    
-    
-      };
+    };
 
     return (
         <div className="chat-container">
             <div className="input-container">
-                <input type="text" className="chat-input" name='getStartedPrompts' onChange={handleChange} placeholder="Type your message here..." />
-                <button className="send-button"  onClick={handleFormSubmit}></button>
+                <input
+                    type="text"
+                    className="chat-input"
+                    name='getStartedPrompts'
+                    onChange={handleChange}
+                    placeholder="Type your message here..."
+                    value={inputValue}  // Control the input with inputValue
+                />
+                <button className="send-button" onClick={handleFormSubmit}></button>
             </div>
         </div>
     );
